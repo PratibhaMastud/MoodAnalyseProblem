3d1bd1101be5cfaa4a37c1b9a493b48b266ba3ce
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-11 01:13:36 +0530

    [Pratibha Mastud] Add: Ability to analyse mood HAPPY or SAD

---
+++ MoodAnalyserProject/MoodAnalyserProject/MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MoodAnalyserProject
+{
+    public class MoodAnalyser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser");
+        }
+        public string analyseMood(string mood)
+        {
+            if (mood.Equals("Happy"))
+            {
+                return "HAPPY";
+            }
+            else
+            {
+                return "SAD";
+            }
+        }
+    }
+}
